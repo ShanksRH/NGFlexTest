b75ce16c10d6524485b697a4124c26cb387f7ead
ShanksRH <epif-sergey@yandex.ru>
2021-04-02 02:26:28 +0300

    remove server

--- server/router/api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-['download', 'upload'].map((route) => {
-	router.use(`/${route}`, require(`./${route}`));
-});
-
-module.exports = router;
